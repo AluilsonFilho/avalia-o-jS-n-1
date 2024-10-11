1998b1529e2f6a086d71ad9abeeac71c804170d6
Aluilson Bezerra Araujo Filho <aluilsonarauo@gmail.com>
2024-10-11 16:08:50 -0300

    Resolução do exercício 1 - Calculadora

---
+++ ava.js
@@ -0,0 +1,23 @@
+function calculadora(num1, num2, operacao) {
+  let resultado;
+
+  if (operacao === "soma") {
+    resultado = num1 + num2;
+  } else if (operacao === "subtracao") {
+    resultado = num1 - num2;
+  } else if (operacao === "multiplicacao") {
+    resultado = num1 * num2;
+  } else if (operacao === "divisao") {
+    if (num2 !== 0) {
+      resultado = num1 / num2;
+    } else {
+      return "Erro: divisão por zero!";
+    }
+  } else {
+    return "Operação inválida!";
+  }
+
+  return resultado;
+}
+
+console.log(calculadora(10, 5, "soma")); // 15
